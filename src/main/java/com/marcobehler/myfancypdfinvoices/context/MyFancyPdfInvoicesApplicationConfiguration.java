228b88ba6b1dadb5ffaf0a0cc14ee2cdcf40705e
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-09-21 19:51:44 +0700

    add @ComponentScan

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -5,21 +5,13 @@ import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
 import com.marcobehler.myfancypdfinvoices.services.UserService;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan
 public class MyFancyPdfInvoicesApplicationConfiguration {
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public UserService userService() {
-        return new UserService();
-    }
-
-    @Bean
-    public InvoiceService invoiceService(UserService userService) {
-        return new InvoiceService(userService);
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
