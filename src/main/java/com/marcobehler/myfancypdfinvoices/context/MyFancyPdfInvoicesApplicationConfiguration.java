d38e932abc189a72ca0d7c228e7b9bc5184defce
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-09-21 19:58:29 +0700

    set basePackageClass for @ComponentScan

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -1,16 +1,13 @@
 package com.marcobehler.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
-import com.marcobehler.myfancypdfinvoices.services.UserService;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import com.marcobehler.myfancypdfinvoices.ApplicationLauncher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackageClasses = ApplicationLauncher.class)
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
