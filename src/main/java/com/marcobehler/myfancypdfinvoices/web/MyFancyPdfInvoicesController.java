f77c4d51062b0bdb45bb847a147508500facb8b2
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-10-04 21:38:04 +0700

    use @RestControllers

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,32 @@
+package com.marcobehler.myfancypdfinvoices.web;
+
+import com.marcobehler.myfancypdfinvoices.dto.InvoiceDto;
+import com.marcobehler.myfancypdfinvoices.model.Invoice;
+import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyFancyPdfInvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+}
